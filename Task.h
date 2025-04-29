ebd8e6dcf51f35aa3ad9c4a5b1e2b6f186d489cb
João Daniel De Cerqueira Lisboa <joaodan.lisboa@gmail.com>
2025-04-28 21:00:17 -0300

    Add files via upload

---
+++ Task.h
@@ -0,0 +1,26 @@
+#ifndef Task.h
+#define Task.h
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <locale.h>
+
+#define Max_tarefas 100
+#define tam_descricao 121
+#define tam_nome 51
+#define tam_validade 9
+
+typedef struct {
+    char nome[tam_nome];
+    char descricao[tam_descricao];
+    int inportancia;
+    char validade[tam_validade];
+} StrTarefas;
+
+void add_tarefa(StrTarefas Tarefas[], int *tarefas_registradas);
+void listar_tarefa(StrTarefas Tarefas[], int tarefas_registradas);
+void apagar_tarefa(StrTarefas Tarefas[], int *tarefas_registradas);
+
+#endif
